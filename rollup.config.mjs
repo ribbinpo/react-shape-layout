596c433513d9df748bcaffca933b0146b83d5497
ribbinpo <ribbinpo@gmail.com>
2023-08-20 11:12:29 +0700

    build: :wrench: rollup pipeline
    add a some library & script file & setup rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,40 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import dts from "rollup-plugin-dts";
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript(),
+      postcss({
+        modules: true,
+        extract: true,
+      }),
+    ],
+    external: [...Object.keys(packageJson.peerDependencies || {})],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/types.d.ts", format: "es" }],
+    plugins: [dts.default()],
+  },
+];
